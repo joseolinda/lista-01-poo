c7609f324bf8806f006276f926cf2b940e77d075
José Olinda <joseolindad@yahoo.com.br>
2021-06-17 16:28:35 -0300

    Arquivos da aula passada

---
+++ Animal.java
@@ -0,0 +1,18 @@
+package aula03;
+
+public class Animal {
+ String nome;
+ String cor;
+ float peso;
+ String especie;
+ int idade;
+ 
+ void voar(){
+   System.out.println("Eu consigo voar");
+ }
+ 
+ void correr(){
+   System.out.println("Estou correndo");
+ }
+
+}
---
+++ Aula03.java
@@ -0,0 +1,11 @@
+package aula03;
+
+public class Aula03 {
+
+  public static void main(String[] args) {
+   Animal gato = new Animal();
+   gato.nome = "Miau";
+   
+   
+  }
+}
